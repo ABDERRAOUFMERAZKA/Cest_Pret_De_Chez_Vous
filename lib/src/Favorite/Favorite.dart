9beda7113f184c9e04b505ba4923fdc34c1885a1
ABDERRAOUFMERAZKA <abderraouf_merazka@hotmail.fr>
2019-10-21 13:07:13 +0200

    as a dev i want to create navigation

---
+++ lib/src/Favorite/Favorite.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../styles.dart';
+
+class Favorite extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Favorite Screen',
+                    textAlign: TextAlign.start, style: Styles.headerLarge),
+              ],
+            )
+          ])),
+    );
+  }
+}
---
+++ lib/src/Search/Search.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../styles.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Search Screen',
+                    textAlign: TextAlign.start, style: Styles.headerLarge),
+              ],
+            )
+          ])),
+    );
+  }
+}
