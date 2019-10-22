1b22aef59c51108f48b42d161c399b4b6b0fd7a7
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-10-22 12:29:25 +0200

    added comments for functions and String Id for Ads

--- lib/api_calls/fetch_ads.dart
+++ lib/api_calls/fetch_ads.dart
@@ -1,11 +1,15 @@
 part of './api_calls.dart';
 
 class FetchAds {
+  /// fetch function for home screen
+  /// returns latest ads in provided location
   static Future<List<Ad>> loadHome({@required String locationAsNumber}) async {
     return dummyListOfAds
         .where((Ad ad) => ad.locationAsNumber == locationAsNumber);
   }
 
+  /// fetch function for search screen
+  /// returns latest ads in provided location and satisfying parameters
   static Future<List<Ad>> search(
       {@required Category category,
       @required List<String> keyWords,
--- lib/api_calls/post_ads.dart
+++ lib/api_calls/post_ads.dart
@@ -1,13 +1,16 @@
 part of './api_calls.dart';
 
 class PostAds {
+  /// function for posting a new ad from a valid Ad object
   /// throws BadParamException if Parameters are not valid
   /// and HttpException if the server gives an error
-  static Future postAd(Ad ad) async {
+  static Future postNewAd(Ad ad) async {
     String email = ad.email;
     String phoneNumber = ad.phoneNumber;
     if (email == null && phoneNumber == null)
       throw new BadParameterException(
           "email and phone number cant be both null");
   }
+
+  static Future deleteAdFromIdStr(String adIdStr) async {}
 }
