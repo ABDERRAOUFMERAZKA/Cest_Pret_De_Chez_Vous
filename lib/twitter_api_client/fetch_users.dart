57032094f627637864213e08338fbadbc8793c0b
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-10-31 16:04:51 +0100

    removed unused files

--- lib/twitter_api_client/fetch_users.dart
+++
@@ -1,8 +0,0 @@
-// part of './api_calls.dart';
-
-// class FetchUsers {
-//   /// fetch a single user from its login
-//   static Future<User> fetchUser(String login) async {
-//     return users.firstWhere((User user) => user.login == login);
-//   }
-// }
