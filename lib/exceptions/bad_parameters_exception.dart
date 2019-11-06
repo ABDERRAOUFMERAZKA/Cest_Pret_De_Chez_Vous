05bd8f6a7126d75591da6134160e8d089f2d2fa0
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-11-06 16:57:12 +0100

    removed exceptions and add Maybe

--- lib/exceptions/bad_parameters_exception.dart
+++
@@ -1,7 +0,0 @@
-class BadParameterException implements Exception {
-  final String errorMessage;
-  BadParameterException([this.errorMessage]);
-
-  String errMsg() =>
-      errorMessage ?? 'The request doesn\'t have the requested parameters';
-}
