2bc24d0dc148a0f482a4a50369d30089ecfce508
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-11-08 17:55:57 +0100

    WIP does not work

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import './Navigation/Navigation.dart';
-import './src/login/login.dart';
+
+import './navigation/Navigation.dart';
+import './src/account_settings/view/login.dart';
 
 class Welcome extends StatelessWidget {
   Widget build(BuildContext context) {
