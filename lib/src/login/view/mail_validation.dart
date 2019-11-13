31fce160d244de7e014a0b6801610bdb0be69cc1
ABDERRAOUFMERAZKA <abderraouf_merazka@hotmail.fr>
2019-11-13 14:45:58 +0100

    add verification

---
+++ lib/src/login/view/mail_validation.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../styles.dart';
+
+class ValidationMail extends  StatefulWidget {
+  @override
+  _VerificationPageState createState() => _VerificationPageState();
+}
+
+class _VerificationPageState extends State<ValidationMail> {
+  String _email;
+
+  @override
+  void initState() {
+    super.initState();
+    getEmail();
+  }
+
+  getEmail() async{
+    var currentUser = await FirebaseAuth.instance.currentUser();
+    var currentUserMail = currentUser.email;
+    print('mail$currentUser');
+    setState(() {
+      _email = currentUserMail;
+    });
+  }
+  Widget build(BuildContext context) {
+    loginButton(String text, onPressed) => Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.deepOrange,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 10.0, 15.0),
+        onPressed: onPressed,
+        child: Text(text,
+            textAlign: TextAlign.center,
+            style: Styles.mediumText.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    validateMail() async {
+      var currentUser = await FirebaseAuth.instance.currentUser();
+      currentUser.sendEmailVerification();
+    }
+
+    logOut() {
+      FirebaseAuth.instance.signOut();
+    }
+
+    return Scaffold(
+      body: Container(
+          child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text('Please Check E-mail of Validation in this adress $_email',
+                            textAlign: TextAlign.start, style: Styles.textDefault),
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        loginButton("Send Email",
+                                () => validateMail()),
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        loginButton("Logout",
+                                () => logOut()),
+                      ],
+                    )
+                  ])
+          )),
+    );
+  }
+}
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,15 +1,22 @@
+import './src/account_settings/view/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-import './navigation/Navigation.dart';
-import './src/account_settings/view/login.dart';
+import './Navigation/Navigation.dart';
+import 'package:cest_pret_de_chez_vous/src/login/view/mail_validation.dart';
 
 class Welcome extends StatelessWidget {
+  getLoggedContent(data) {
+    if (data == null) {
+      return Login();
+    } else if(data.isEmailVerified) {
+      return Tabs();
+    } return ValidationMail();
+  }
   Widget build(BuildContext context) {
     return StreamBuilder<FirebaseUser>(
       stream: FirebaseAuth.instance.onAuthStateChanged,
       builder: (context, snapshot) {
-        return snapshot.data != null ? Tabs() : Login();
+        return getLoggedContent(snapshot.data);
       },
     );
   }
