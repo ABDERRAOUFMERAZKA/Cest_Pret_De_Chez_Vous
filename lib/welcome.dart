fc8c5432eeed7ac158762d1258280ba29a147e99
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-11-20 10:53:19 +0100

    navigation not Navigation

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,17 +1,19 @@
 import './src/account_settings/view/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import './Navigation/Navigation.dart';
+import './navigation/navigation.dart';
 import 'package:cest_pret_de_chez_vous/src/login/view/mail_validation.dart';
 
 class Welcome extends StatelessWidget {
   getLoggedContent(data) {
     if (data == null) {
       return Login();
-    } else if(data.isEmailVerified) {
+    } else if (data.isEmailVerified) {
       return Tabs();
-    } return ValidationMail();
+    }
+    return ValidationMail();
   }
+
   Widget build(BuildContext context) {
     return StreamBuilder<FirebaseUser>(
       stream: FirebaseAuth.instance.onAuthStateChanged,
