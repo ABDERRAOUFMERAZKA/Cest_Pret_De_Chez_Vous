5b8f3220ebbcdb8bde1eedb9c6428b946b059566
Bapth-dej <baptiste.desjobert@outlook.fr>
2019-11-25 12:48:11 +0100

    remove unused import and package

--- lib/src/display_ads/service/retrieve_ads.dart
+++ lib/src/display_ads/service/retrieve_ads.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:cest_pret_de_chez_vous/utils/list_utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
